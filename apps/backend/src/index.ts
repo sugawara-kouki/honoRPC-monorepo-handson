0326d4903ef0fe68f1791a0f8725605488da0bb8
sugawara-kouki <kou.720231@gmail.com>
2025-03-08 23:57:31 +0900

    wip: honoRPCとモノレポ構成試している

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,34 @@
+import { zValidator } from '@hono/zod-validator'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { z } from 'zod'
+
+const app = new Hono()
+
+// frontendからAPIを使用できるように、CORSを許可する
+app.use('*', cors({
+  origin: "*"
+}))
+
+// todoエントリーへのリクエストパラメータのバリデーションスキーマ定義
+const todoSchema = z.object({
+  title: z.string().min(2),
+  description: z.string().nullable(),
+})
+
+const route = app
+  .get('/hello', (c) => {
+    return c.json({message: "Hello Hono!"})
+  })
+  .post('/todo', zValidator('json', todoSchema, (result, c) => {
+    if (!result.success) {
+      return c.text(result.error.issues[0].message, 400)
+    }
+  }),
+  (c) => {
+    const {title, description} = c.req.valid('json')
+    return c.json({title, description})
+  })
+
+export type AppType = typeof route
+export default app
